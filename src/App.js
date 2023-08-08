2b8788bc179a8dff61893ad4e232f2b6e4e75d37
Broni27 <broni.27@mail.ru>
2023-08-08 15:03:32 +0600

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import "./styles/App.css";
+import logo from "./images/logo.png";
 function App() {
   return (
-    <div className="App" value={}>
-      <div className={'tBlock'}>
-        <p></p>
-      </div>
+    <div className="App">
+        <header className="headerStyle">
+            <img className="logoImage" src= {logo} alt="logo.png"/>
+            <nav className="navStyle">
+                <a href="#" className="linkStyle">Главная</a>
+                <a href="/games" className="linkStyle">Игры</a>
+                <a href="/signIn" className="linkStyle">Войти</a>
+            </nav>
+        </header>
     </div>
   );
 }
