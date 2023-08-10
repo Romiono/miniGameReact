f28d988c7b636f1f3d167983d0438355258539a4
Infekted <mr.freze.roma@mail.ru>
2023-08-11 02:05:40 +0300

    поборол таки эти модули с картинками, с about закончили

--- src/components/textBlocks/Tblock2.jsx
+++ src/components/textBlocks/Tblock2.jsx
@@ -5,8 +5,8 @@ import paper from "../../images/vector.png";
 const Tblock2 = ({title, value, img}) => {
 
     return (
-        <div className={classes.all} style={{width: '100%', bottom: "350px"}}>
-            <img className={[classes.paperInv, classes.Paper].join(' ')} src={paper} alt={'paper.png'} style={{top: '10px'}}/>
+        <div className={classes.all} style={{width: '100%'}}>
+            {/*<img className={[classes.paperInv, classes.Paper].join(' ')} src={paper} alt={'paper.png'} style={{top: '10px'}}/>*/}
             <div className={classes.tBlock}>
                 <img src={img} className={classes.hexagone} alt={'D.png'}/>
                 <div>
@@ -14,7 +14,7 @@ const Tblock2 = ({title, value, img}) => {
                     <p className={classes.mainText}>{value}</p>
                 </div>
             </div>
-            <img className={classes.Paper} src={paper} alt={'paper.png'}/>
+            {/*<img className={classes.Paper} src={paper} alt={'paper.png'}/>*/}
         </div>
     );
 };
